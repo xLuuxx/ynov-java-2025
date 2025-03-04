c7d5d389a65f8675b3957acb4141f10581a9ea62
Camuset Laurine <laurine.camuset@ynov.com>
2025-03-04 16:47:42 +0100

    feat(Arrays) printing all elements of the arrays once

---
+++ java_exercice/src/fr/ynov/java/easy/Arrays.java
@@ -0,0 +1,10 @@
+package fr.ynov.java.easy;
+
+public class Arrays {
+   public static void main(String[] args) {
+       int[] arr = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+       for (int i = 0; i < 1; i++) {
+           System.out.println(java.util.Arrays.toString(arr));
+       }
+   }
+}
