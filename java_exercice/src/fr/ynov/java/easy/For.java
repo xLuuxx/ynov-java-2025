9cb7efed3915f2dd7e5087bea9350a9fc1674f68
Camuset Laurine <laurine.camuset@ynov.com>
2025-03-04 16:30:58 +0100

    feat(Loops) printed from 1 to 10 using for, while and do while.

---
+++ java_exercice/src/fr/ynov/java/easy/For.java
@@ -0,0 +1,9 @@
+package fr.ynov.java.easy;
+
+public class For {
+    public static void main(String[] args) {
+        for (int i = 1; i < 11; i++) {
+            System.out.println(i);
+        }
+    }
+}
