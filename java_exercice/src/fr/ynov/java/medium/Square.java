6b25e9b014a924485bb7b8d2085416587a322476
Camuset Laurine <laurine.camuset@ynov.com>
2025-03-14 14:58:10 +0100

    feat(square) now working

--- java_exercice/src/fr/ynov/java/medium/Square.java
+++ java_exercice/src/fr/ynov/java/medium/Square.java
@@ -4,16 +4,16 @@ import java.util.Scanner;
 
 public class Square {
 
-    private static int mathsSquare(int number) {
-        return number * number;
-    }
-
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Enter a number:");
         int number = scanner.nextInt();
-        int result = mathsSquare(number);
+        int result = square(number);
         System.out.println("The square of " + number + " is: " + result);
+    }
+
+    public static int square(int number) {
+        return number * number;
     }
 }
 
